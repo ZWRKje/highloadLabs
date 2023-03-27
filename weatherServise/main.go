7c070ef84a904235f3b8e8cf90dd518ab5e53420
ZWRKje <andrey09767@gmail.com>
2023-03-27 22:08:57 +0300

    feat: add forecast

--- weatherServise/main.go
+++ weatherServise/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 type weatherData struct {
@@ -12,6 +13,17 @@ type weatherData struct {
 	Main struct {
 		Temp float64 `json:"temp`
 	} `json:main`
+}
+
+type forecastWeatherData struct {
+	List []struct {
+		Main struct {
+			Temp float64 `json:"temp`
+		} `json:main`
+	} `json:list`
+	City struct {
+		Name string `json:name"`
+	} `json:city"`
 }
 
 type wetherResp struct {
@@ -41,6 +53,30 @@ func main() {
 		json.NewEncoder(w).Encode(resp)
 	})
 
+	http.HandleFunc("/v1/forecast/", func(w http.ResponseWriter, req *http.Request) {
+		city := req.URL.Query().Get("city")
+		dt := req.URL.Query().Get("dt")
+		i, err := strconv.ParseInt(dt, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		url := os.Getenv("URL")
+		url = fmt.Sprintf("%sforecast/?q=%s&appid=%s&units=metric&cnt=%s", url, city, apiKey, dt)
+		// fmt.Print(url)
+		data, err := forecast(url, i)
+		resp := wetherResp{data.City.Name, "celsius", data.List[i-1].Main.Temp}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		json.NewEncoder(w).Encode(resp)
+	})
+
 	fmt.Printf("%s \n", port)
 	fmt.Println("Server is listening...")
 	http.ListenAndServe("localhost:"+port, nil)
@@ -58,5 +94,21 @@ func current(url string) (weatherData, error) {
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
 		return weatherData{}, err
 	}
+	return d, nil
+}
+
+func forecast(url string, ts int64) (forecastWeatherData, error) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	var d forecastWeatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return forecastWeatherData{}, err
+	}
+
 	return d, nil
 }
