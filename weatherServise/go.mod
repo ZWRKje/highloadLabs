cd008df849528a21d01816880e099d34a1e82c3d
ZWRKje <andrey09767@gmail.com>
2023-05-18 01:25:41 +0300

    feat: added monitoring

--- weatherServise/go.mod
+++ weatherServise/go.mod
@@ -2,12 +2,21 @@ module weather
 
 go 1.18
 
-require google.golang.org/grpc v1.54.0
+require (
+	github.com/prometheus/client_golang v1.15.1
+	github.com/redis/go-redis/v9 v9.0.4
+	github.com/slok/go-http-metrics v0.10.0
+	google.golang.org/grpc v1.54.0
+)
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/redis/go-redis/v9 v9.0.4 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
--- weatherServise/main.go
+++ weatherServise/main.go
@@ -5,14 +5,33 @@ import (
 	"net/http"
 
 	"weather/handler"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
+	"github.com/slok/go-http-metrics/middleware"
+	middlewarestd "github.com/slok/go-http-metrics/middleware/std"
 )
 
 func main() {
+
+	mdlw := middleware.New(middleware.Config{
+		Recorder: metrics.NewRecorder(metrics.Config{}),
+	})
 	h := handler.NewHandler()
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", h.HelloServer)
-	http.HandleFunc("/v1/current/", h.CurrentWeather)
-	http.HandleFunc("/v1/forecast/", h.ForecastWeather)
-	http.HandleFunc("/v1/save/", h.SaveWeatherData)
+
+	hFuncCurent := http.HandlerFunc(h.CurrentWeather)
+	hCurent := middlewarestd.Handler("my_mytrics", mdlw, hFuncCurent)
+	http.Handle("/v1/current/", hCurent)
+
+	hFuncForecast := http.HandlerFunc(h.ForecastWeather)
+	hForecast := middlewarestd.Handler("my_mytrics", mdlw, hFuncForecast)
+	http.Handle("/v1/forecast/", hForecast)
+
+	hFuncSave := http.HandlerFunc(h.SaveWeatherData)
+	hSave := middlewarestd.Handler("my_mytrics", mdlw, hFuncSave)
+	http.Handle("/v1/save/", hSave)
 	fmt.Printf("%s \n", h.Port)
 	fmt.Println("Server is listening at localhost:" + h.Port)
 	err := http.ListenAndServe("0.0.0.0:"+h.Port, nil)
